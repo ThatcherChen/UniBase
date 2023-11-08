7ad2e431a41d545b322c25c27e17150203d109db
Flash-Z <65103912+Flash-Z@users.noreply.github.com>
2023-11-08 20:01:03 +0800

    Update quick-start.bat
    经同学反馈，之前的有误

--- misc/quick-start.bat
+++ misc/quick-start.bat
@@ -4,7 +4,7 @@ REM 克隆 UniBase 仓库
 git clone --recursive https://github.com/ThatcherChen/UniBase.git
 
 REM 启动 Docker 容器
-docker run -itd -v "%cd%\UniBase:/mnt/UniBase" --name unibase-env-test base-env
+docker run -itd -v "%cd%\UniBase:/mnt/UniBase" --name unibase-env base-env
 
 REM 进入 Docker 容器
 docker exec -it unibase-env bash -c "cd /mnt/UniBase && mkdir build && cd build && cmake .. -DCMAKE_BUILD_TYPE=Debug"
